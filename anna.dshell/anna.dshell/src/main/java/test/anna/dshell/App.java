9857a492d51924ffaf9f0c0bc22c19334c50d2b4
rizvi <mrhasan@kth.se>
2015-10-14 15:57:39 +0200

    anna.dshell Initial commit

---
+++ anna.dshell/anna.dshell/src/main/java/test/anna/dshell/App.java
@@ -0,0 +1,13 @@
+package test.anna.dshell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
